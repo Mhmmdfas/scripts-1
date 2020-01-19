e330f13250cfa221b44d8ff6b2e9ccedead9362b
fadlyas07 <fadlyardhians@gmail.com>
2020-01-19 12:56:25 +0700

    Add pararel build scripts
    Signed-off-by: fadlyas07 <fadlyardhians@gmail.com>

---
+++ paralel/rolex.sh
@@ -0,0 +1,128 @@
+#!/usr/bin/env bash
+git clone -q https://github.com/fabianonline/telegram.sh telegram
+	TELEGRAM=telegram/telegram
+	KERNEL_NAME="GREENFORCE"
+	TELEGRAM_ID=${chat_id}
+	TELEGRAM_TOKEN=${token}
+	export TELEGRAM_TOKEN
+	export KERNEL_TYPE
+	export ARCH=arm64
+	export SUBARCH=arm64
+	export KBUILD_BUILD_USER=MhmmdFadlyas
+	export KBUILD_BUILD_HOST=GF-LaB
+tg_channelcast() {
+    "${TELEGRAM}" -c "${TELEGRAM_ID}" -H \
+    "$(
+		for POST in "${@}"; do
+			echo "${POST}"
+		done
+    )"
+}
+sed_template() {
+tg_channelcast "<b>${KERNEL_NAME} Build Failed</b>!!" \
+										"<b>Build took</b> <code>$((${DIFF1} / 60)) minute(s) and $((${DIFF1} % 60)) second(s)</code> <b>before failed!</b>"
+}
+clean_build() {
+rm -rf out ${ZIP_DIR} ${TOOLCHAIN_DIR}/${TOOLCHAIN_DIRNAME} ${TEMP}/*.log
+}
+make_proton() {
+	export LD_LIBRARY_PATH="${TOOLCHAIN_DIR}/${TOOLCHAIN_DIRNAME}/bin/../lib:$PATH"
+	make -s -C "${KERNEL_DIR}" -j$(nproc) O=out ARCH=arm64 ${KERNEL_CONFIG}
+	PATH="${TOOLCHAIN_DIR}/${TOOLCHAIN_DIRNAME}/bin:${PATH}" \
+	make -C "${KERNEL_DIR}" -j$(nproc) -> ${TEMP}/rolex.log O=out \
+                  					ARCH=arm64 \
+                  					CC=clang \
+							CLANG_TRIPLE=aarch64-linux-gnu- \
+							CROSS_COMPILE=aarch64-linux-gnu- \
+							CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+}
+make_clangaosp() {
+	export KBUILD_COMPILER_STRING="$(${TOOLCHAIN_DIR}/${TOOLCHAIN_DIRNAME}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
+	make -s -C "${KERNEL_DIR}"-j$(nproc) O=out ARCH=arm64 ${KERNEL_CONFIG}
+	PATH="${TOOLCHAIN_DIR}/${TOOLCHAIN_DIRNAME}/bin:${TOOLCHAIN_DIR}/gcc/bin:${TOOLCHAIN_DIR}/gcc32/bin:${PATH}" \
+	make -s -C "${KERNEL_DIR}" -j$(nproc) -> ${TEMP}/rolex.log O=out \
+							ARCH=arm64 \
+							CC=clang \
+							CLANG_TRIPLE=aarch64-linux-gnu- \
+							CROSS_COMPILE=aarch64-linux-android- \
+							CROSS_COMPILE_ARM32=arm-linux-androideabi-
+}
+make_gcc() {
+	make -s -C "${KERNEL_DIR}" -j$(nproc) O=out ARCH=arm64 ${KERNEL_CONFIG}
+	PATH="${TOOLCHAIN_DIR}/gcc/bin:${TOOLCHAIN_DIR}/gcc32/bin:${PATH}" \
+	make -s -C "${KERNEL_DIR}" -j$(nproc) -> ${TEMP}/rolex.log O=out \
+							ARCH=arm64 \
+							CROSS_COMPILE=aarch64-linux-android- \
+							CROSS_COMPILE_ARM32=arm-linux-androideabi-
+}
+KERNEL_DIR="$(pwd)"
+mkdir $KERNEL_DIR/4A
+TEMP="$KERNEL_DIR/4A"
+KERNEL_IMG="$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb"
+KERNEL_DEVICE="Xiaomi Redmi 4A"
+KERNEL_CONFIG="rolex_defconfig"
+CODENAME_DEVICE="rolex"
+TOOLCHAIN_DIR="/root/toolchain"
+ZIP_DIR="$KERNEL_DIR/${CODENAME_DEVICE}"
+git clone -j32 --depth=1 https://github.com/Mhmmdfas/anykernel3 -b ${CODENAME_DEVICE} ${CODENAME_DEVICE}
+echo "clone ${CODENAME_DEVICE}"
+main_uts() {
+KERNEL_UTS_VERSION=$(cat ${KERNEL_DIR}/out/include/generated/compile.h | grep UTS_VERSION | cut -d '"' -f2)
+}
+push_log() {
+	curl -F document=@$(echo ${TEMP}/*.log) "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendDocument" \
+	-F chat_id="${fadlyas}"
+}
+push() {
+	curl -F document=@$(echo ${ZIP_DIR}/*.zip) "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendDocument" \
+	-F chat_id="${TELEGRAM_ID}" \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=html" \
+	-F caption="Build took $(($DIFF1 / 60)) minute(s) and $(($DIFF1 % 60)) second(s). <b>For ${KERNEL_DEVICE}</b> [ <code>$KERNEL_UTS_VERSION</code> ]"
+}
+DATE1=$(TZ=Asia/Jakarta date +'%H%M-%d%m%y')
+BUILD_START1=$(date +"%s")
+if [[ "$PARSE_BRANCH" == "HMP" ]];
+then
+	KERNEL_TYPE=HMP
+	export $KERNEL_TYPE
+	TOOLCHAIN_DIRNAME="clang"
+	export $TOOLCHAIN_DIRNAME
+	make_clangaosp
+elif [[ "$PARSE_BRANCH" == "EAS" ]];
+then
+	KERNEL_TYPE=EAS
+	export $KERNEL_TYPE
+	make_gcc
+elif [[ "$PARSE_BRANCH" == "turn-on" ]];
+then
+	KERNEL_TYPE=PURE-CAF
+	export $KERNEL_TYPE
+	TOOLCHAIN_DIRNAME="proton"
+	export $TOOLCHAIN_DIRNAME
+	make_proton
+elif [[ ! "$KERNEL_TYPE" ]];
+then
+	KERNEL_TYPE=TEST
+	echo "Maybe i'm on test-build/scripts right now!"
+	export $KERNEL_TYPE
+	TOOLCHAIN_DIRNAME="proton"
+	export $TOOLCHAIN_DIRNAME
+	make_proton
+fi
+main_uts
+BUILD_END1=$(date +"%s")
+DIFF1=$((${BUILD_END1} - ${BUILD_START1}))
+if [[ ! -f "${KERNEL_IMG}" ]];
+then
+	push_log
+        sed_template
+	exit 1;
+fi
+cd ${ZIP_DIR}
+cp ${KERNEL_IMG} ${ZIP_DIR}/zImage
+zip -r9q ${KERNEL_NAME}-${KERNEL_TYPE}-${CODENAME_DEVICE}-${DATE1}.zip * -x .git README.md LICENCE
+push
+push_log
+cd ~/project/
+clean_build
---
+++ paralel/sendinfo.sh
@@ -0,0 +1,106 @@
+ #
+ # Script For Building Android arm64 Kernel
+ #
+ # Copyright (C) 2019-2020 Muhammad Fadlyas (@Mhmmdfas)
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #	 http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ #
+#!/usr/bin/env bash
+export TZ=":Asia/Jakarta"
+
+export USE_CCACHE=1
+export CACHE_DIR=~/.ccache
+export STICKER
+export KERNEL_TYPE
+
+	git clone -q https://github.com/fabianonline/telegram.sh telegram
+	echo -c "clone Telegram.."
+
+TELEGRAM=telegram/telegram
+TELEGRAM_ID=${chat_id}
+TELEGRAM_TOKEN=${token}
+export TELEGRAM_TOKEN
+
+# Github Env Vars
+KERNEL_NAME="GREENFORCE"
+UNIFIED="Xiaomi Redmi 4A & 5A"
+PARSE_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+COMMIT_POINT="$(git log --pretty=format:'%h: %s by %an' -1)"
+TOOLCHAIN_DIR="/root/toolchain"
+
+if [ "$PARSE_BRANCH" == "HMP" ];
+then
+	KERNEL_TYPE=HMP
+	export $KERNEL_TYPE
+	STICKER="CAADBQADeQEAAn1Cwy71MK7Ir5t0PhYE"
+	export $STICKER
+	TOOLCHAIN_DIRNAME="clang"
+	export $TOOLCHAIN_DIRNAME
+elif [ "$PARSE_BRANCH" == "EAS" ];
+then
+	KERNEL_TYPE=EAS
+	export $KERNEL_TYPE
+	STICKER="CAADBQADIwEAAn1Cwy5pf2It72fNXBYE"
+	export $STICKER
+elif [ "$PARSE_BRANCH" == "turn-on" ];
+then
+	KERNEL_TYPE=PURE-CAF
+	export $KERNEL_TYPE
+	STICKER="CAADBQADfAEAAn1Cwy6aGpFrL8EcbRYE"
+	export $STICKER
+	TOOLCHAIN_DIRNAME="proton"
+	export $TOOLCHAIN_DIRNAME
+elif [ ! "$KERNEL_TYPE" ];
+then
+	KERNEL_TYPE=TEST
+	echo -e "Maybe i'm on test-build/scripts right now!"
+	export $KERNEL_TYPE
+	STICKER="CAADBQADPwEAAn1Cwy4LGnCzWtePdRYE"
+	export $STICKER
+	TOOLCHAIN_DIRNAME="proton"
+	export $TOOLCHAIN_DIRNAME
+fi
+
+tg_channelcast() {
+    "${TELEGRAM}" -c "${TELEGRAM_ID}" -H \
+    "$(
+		for POST in "${@}"; do
+			echo "${POST}"
+		done
+    )"
+}
+
+tg_sendstick() {
+   curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendSticker" \
+	-d sticker="${STICKER}" \
+	-d chat_id="${TELEGRAM_ID}"
+}
+
+tg_sendstick
+
+if [ "$KERNEL_TYPE" == "EAS" ];
+then
+	tg_channelcast "<b>${KERNEL_NAME} ${KERNEL_TYPE} build is available</b>!" \
+				"<b>Device :</b><code> ${UNIFIED} </code>" \
+				"<b>Branch :</b><code> ${PARSE_BRANCH} </code>" \
+				"<b>Toolchain :</b><code> $(${TOOLCHAIN_DIR}/gcc/bin/aarch64-linux-android-gcc --version | head -n 1 ) </code>" \
+				"<b>Latest commit :</b><code> ${COMMIT_POINT} </code>"
+elif [ ! "$KERNEL_TYPE" == "EAS" ];
+then
+	tg_channelcast "<b>${KERNEL_NAME} ${KERNEL_TYPE} build is available</b>!" \
+			"<b>Device :</b><code> ${UNIFIED} </code>" \
+			"<b>Branch :</b><code> ${PARSE_BRANCH} </code>" \
+			"<b>Toolchain :</b><code> $(${TOOLCHAIN_DIR}/${TOOLCHAIN_DIRNAME}/bin/clang --version | head -n 1 | perl -pe 's/\(https.*?\)//gs' | sed -e 's/  */ /g') </code>" \
+			"<b>Latest commit :</b><code> ${COMMIT_POINT} </code>"
+fi
+tg_channelcast "<b>Build Started At </b><code>$(TZ=Asia/Jakarta date)</code>"
